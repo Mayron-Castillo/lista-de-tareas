aa5c770f70041446a18f04e378dd625fe44a9f78
Mayron Castillo <12mayron12@gmail.com>
2025-02-18 11:07:52 -0600

    Arreglo de validación cuando no se añade nada

--- script.js
+++ script.js
@@ -2,24 +2,34 @@ const form = document.querySelector(".form");
 const input = document.querySelector(".input");
 const list = document.querySelector(".agregado");
 
+const errorTexto = document.createElement("p");
+errorTexto.classList.add("error-message");
+form.appendChild(errorTexto);
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const value = input.value.trim(); 
 
-    if (value === "") return; // Evita agregar elementos vacíos
+    if (value === ""){
+        errorTexto.textContent = "⚠️ Por favor, ingresa una tarea.";
+        errorTexto.style.color = "red";
+        return ; // Evita agregar elementos vacíos
+    } 
+
+    errorTexto.textContent = "";
 
     const newTask = document.createElement("li");
     newTask.classList.add("task");
 
-    // Contenedor para el texto de la tarea
+    // texto de la tarea
     const taskText = document.createElement("span");
     taskText.textContent = `- ${value}`;
 
-    // Contenedor para los botones (✅ y 🗑️)
+    // div para boton completar y eliminar
     const buttonContainer = document.createElement("div");
     buttonContainer.classList.add("button-container");
 
-    // Botón para completar tarea (✅)
+    // añade botón completar tarea
     const completeBtn = document.createElement("button");
     completeBtn.textContent = "✅";
     completeBtn.classList.add("action-btn");
@@ -29,25 +39,22 @@ form.addEventListener("submit", (e) => {
         newTask.classList.toggle("completed");
     });
 
-    // Botón para eliminar tarea
+    // añade botón borrar tarea
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "🗑️";
     deleteBtn.classList.add("action-btn");
 
-    // Evento para eliminar la tarea
+    //eliminar tarea
     deleteBtn.addEventListener("click", () => {
         newTask.remove();
     });
 
-    // Agrega los botones al contenedor
     buttonContainer.appendChild(completeBtn);
     buttonContainer.appendChild(deleteBtn);
 
-    // Agregar elementos a la tarea
     newTask.appendChild(taskText);
     newTask.appendChild(buttonContainer);
 
-    // Agregar la tarea a la lista
     list.appendChild(newTask);
 
     input.value = "";
